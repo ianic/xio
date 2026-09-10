5152d816a5632f76dfdb254c194f494ea9697657
Igor Anić <igor.anic@gmail.com>
2026-09-10 17:39:37 +0200

    upgrade to latest zig
    Changed deprecated fmt.bufPrint* to mem.print*, reference:
    https://codeberg.org/ziglang/zig/commit/36dd7e9c375d73e73a1717a451943579a4694c2e#diff-6894329f333b8e67bdf328e04aa9724aca6af33b

--- src/Evented.zig
+++ src/Evented.zig
@@ -4848,7 +4848,7 @@ fn readAll(ev: *Evented, fd: fd_t, buffer: []u8) (File.Reader.Error || error{End
 
 fn realPath(ev: *Evented, fd: fd_t, out_buffer: []u8) File.RealPathError!usize {
     var procfs_buf: [std.fmt.count("/proc/self/fd/{d}\x00", .{std.math.minInt(fd_t)})]u8 = undefined;
-    const proc_path = std.fmt.bufPrintSentinel(&procfs_buf, "/proc/self/fd/{d}", .{fd}, 0) catch
+    const proc_path = std.mem.printSentinel(&procfs_buf, "/proc/self/fd/{d}", .{fd}, 0) catch
         unreachable;
     while (true) {
         try ev.enqueueSync();
